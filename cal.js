55afafe8ba2ddee29c467089110be0e4fe83aede
nik-1207 <nmknikhilkashyap@gmail.com>
2021-08-25 14:55:06 +0530

    final commit

--- cal.js
+++ cal.js
@@ -1,27 +1,51 @@
-function reset()
-{
-    document.getElementById("res").value = ''
-}
-function solve()
-{
-    let x = document.getElementById("res").value
-    let ans=eval(x)
-    if(Number.isFinite(ans))
-    {
-        document.getElementById("res").value = eval(x)
-    }
-    else
-    {        
-        document.getElementById("res").value = "invalid input"
-    }
-}
-function dis(val)
-{
-    document.getElementById("res").value+=val
-}
-function del()
-{
-    let x = document.getElementById("res").value
-    document.getElementById("res").value=x.slice(0,-1)
+dis = (key) => {
+    if (key == "+" || key == "-" || key == "*" || key == "/") {
+        document.getElementById("num1").innerText = document.getElementById("num2").innerText + key
+        document.getElementById("num2").innerText = ""
 
+    } else {
+        document.getElementById("num2").innerText += key
+    }
+
+}
+solve = () => {
+    num1 = document.getElementById("num1").innerText
+    num2 = document.getElementById("num2").innerText
+    operation = num1[num1.length - 1]
+    num1 = num1.substring(0, num1.length - 1)
+    try {
+        if (Number.isInteger(num1)) {
+            num1 = parseInt(num1)
+        } else if (Number(num1) === num1 && num1 % 1 !== 0) {
+            num1 = parseFloat(num1)
+        } else {
+            x = "Invalid Input"
+        }
+        console.log(parseFloat("0.0.1"))
+        x = eval((num1 + operation + num2))
+        if (x == "Infinity" || x == "NAN") {
+            x = "Invalid Input"
+        }
+        document.getElementById("num2").innerText = x
+    } catch (e) {
+
+        console.log(e)
+        document.getElementById("num1").innerText = ""
+        document.getElementById("num2").innerText = "Invalid Input"
+    }
+}
+reset = () => {
+    document.getElementById("num1").innerText = ""
+    document.getElementById("num2").innerText = "0"
+}
+del = () => {
+    console.log(document.getElementById("num2").innerText)
+    if (document.getElementById("num2").innerText === "Invalid Input") {
+        console.log(document.getElementById("num2").innerText)
+        reset()
+    } else {
+        num2 = document.getElementById("num2").innerText;
+        x = num2.substring(0, num2.length - 1)
+        document.getElementById("num2").innerText = x
+    }
 }
